089c4026e2e91bb36f32c472cd7e282ebbf7cb1a
RahulNavneeth <rahulmnavneeth@gmail.com>
2024-04-24 17:02:29 +0530

    init🍥: setup

---
+++ src/lib/components/footer/index.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+        <div className="w-full bg-gray-50 text-center py-2 border-black border-t-[1px]">Rahul M Navneeth</div>
+    )
+}
+
+export default Footer;
